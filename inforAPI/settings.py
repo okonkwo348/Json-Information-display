cd24c19bfc8308a9d63b1418bc4812c789793022
okonkwo348 <105568258+okonkwo348@users.noreply.github.com>
2025-01-31 22:42:27 +0100

    Update settings.py
    updated setting.py

--- inforAPI/settings.py
+++ inforAPI/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-0&rk(cq!gqt=-ur68k4kp^cnwiwc$)v#=i@$fm5+4@8(0d3q2n'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = ['your-render-api.onrender.com']
+ALLOWED_HOSTS = ['json-information-display.onrender.com','localhost','127.0.0.1']
 
 
 # Application definition
